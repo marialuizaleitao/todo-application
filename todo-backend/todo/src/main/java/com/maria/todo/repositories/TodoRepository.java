08cfb4ec706cca9f00d95953b3bab9c0f4567c5f
Maria Leitão <malufln.99@gmail.com>
2024-04-22 16:05:18 -0300

    Implementação do método listOpen

--- todo-backend/todo/src/main/java/com/maria/todo/repositories/TodoRepository.java
+++ todo-backend/todo/src/main/java/com/maria/todo/repositories/TodoRepository.java
@@ -1,11 +1,17 @@
 package com.maria.todo.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.maria.todo.domain.Todo;
 
 @Repository
 public interface TodoRepository extends JpaRepository<Todo, Integer> {
+
+	@Query("SELECT obj FROM Todo obj WHERE obj.finalizado = false ORDER BY obj.dataParaFinalizar ASC")
+	List<Todo> findAllOpen();
 
 }
--- todo-backend/todo/src/main/java/com/maria/todo/services/DBService.java
+++ todo-backend/todo/src/main/java/com/maria/todo/services/DBService.java
@@ -21,7 +21,13 @@ public class DBService {
 
 		Todo t1 = new Todo(null, "Estudar", "Estudar Spring Boot 2 e Angular 11",
 				LocalDateTime.parse("25/03/2022 10:40", formatter), false);
+		Todo t2 = new Todo(null, "Ler", "Ler livro de desenvolvimento pessoal",
+				LocalDateTime.parse("22/03/2021 13:00", formatter), true);
+		Todo t3 = new Todo(null, "Exercícios", "Aula de Kung Fu", LocalDateTime.parse("21/03/2022 19:30", formatter),
+				false);
+		Todo t4 = new Todo(null, "Meditar", "Meditar durante 30 minutos pela manhã",
+				LocalDateTime.parse("27/03/2021 05:40", formatter), true);
 
-		todoRepository.saveAll(Arrays.asList(t1));
+		todoRepository.saveAll(Arrays.asList(t1, t2, t3, t4));
 	}
 }
